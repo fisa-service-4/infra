366b927a6fcd30bdc1d40b5bea0cb9a32e4a2d44
hyun7931 <119102748+hyun7931@users.noreply.github.com>
2026-06-07 21:08:02 +0900

    feat(infra): deploy.sh 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,20 @@
 # deploy.sh
+#!/bin/bash
+
+set -e
+
+cd ~/infra
+
+git pull origin develop
+
+docker compose \
+  -f docker-compose-core.yml \
+  -f docker-compose-app.yml \
+  pull
+
+docker compose \
+  -f docker-compose-core.yml \
+  -f docker-compose-app.yml \
+  up -d
+
+docker image prune -f
